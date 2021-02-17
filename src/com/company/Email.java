ac60350407c4a3c6dd4c6546917f6b0e65ed8015
lauralevy510 <llevy510@gmail.com>
2021-02-17 09:26:51 -0500

    added 6 todo items to email.java

--- src/com/company/Email.java
+++ src/com/company/Email.java
@@ -8,5 +8,16 @@ public class Email {
     private int mailboxCapacity;
     private String alertnateEmail;
 
+    //TODO: constructor to receive the firstname and lastname
+
+    //TODO: ask for the department
+
+    //TODO: generate a random password
+
+    //TODO: set the mailbox capacity
+
+    //TODO: set alternate email
+
+    //TODO: change the email
 
 }

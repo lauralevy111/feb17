5fea488fefc98260259d9af61bf20a0cfd3e4447
lauralevy510 <llevy510@gmail.com>
2021-02-17 10:01:01 -0500

    created randomPassword method that returns randompassword

--- src/com/company/Email.java
+++ src/com/company/Email.java
@@ -36,7 +36,19 @@ public class Email {
         return department;
     }
 
-    //TODO: generate a random password
+    //generate a random password
+    private String randomPassword(int length){
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%";
+        char[] password = new char[length];
+
+        for (int i = 0; i < length; i++){
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] =passwordSet.charAt(rand);
+        }
+
+        return new String(password);
+
+    }
 
     //TODO: set the mailbox capacity
 

6aa463a71f6d6541cd9b9cfd4e538492fca27e08
lauralevy510 <llevy510@gmail.com>
2021-02-17 09:25:28 -0500

    added email class and initiated a bunch of variables in that class.

--- src/com/company/Email.java
+++ src/com/company/Email.java
@@ -1,2 +1,12 @@
-package com.company;public class Email {
+package com.company;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alertnateEmail;
+
+
 }

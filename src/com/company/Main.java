37e6d978fa2905de48d136e47f097484fdb7df67
lauralevy510 <llevy510@gmail.com>
2021-02-18 08:02:39 -0500

    cleanign up main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,5 @@ public class Main {
     public static void main(String[] args) {
         Email em1 = new Email("Harry","Styles");
 
-        em1.setAlternateEmail("hs@syco.com");
-        System.out.println(em1.getAlternateEmail());
     }
 }

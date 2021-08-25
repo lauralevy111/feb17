592da73d2cb696832978fb09c2ea81ff2b57793f
lauralevy510 <llevy510@gmail.com>
2021-08-25 11:52:17 -0400

    commented out things for scratch work on 8/25

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        /*
         Email em1 = new Email("Harry","Styles");
         System.out.println(em1.showInfo());
+        */
+
+        System.out.println("laura levy 8/25");
+        
+
     }
 }
